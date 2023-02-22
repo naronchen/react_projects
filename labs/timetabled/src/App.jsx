417be89deb80b2779ef7d9bed4f69d9bf5929c79
Naron <naron.chen@richmond.edu>
2023-02-21 22:17:58 -0500

    Initial commit

---
+++ labs/timetabled/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Calendar from './components/Calendar'
+
+const App = () => {
+
+  return (
+    <div className="App">
+    <h1>time table🥑 </h1>
+    <h2>welcome</h2>
+    <Calendar />
+    </div>
+  )
+}
+
+export default App
