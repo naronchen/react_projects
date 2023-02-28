ff0415c390c54dc0c114670fa361f89c2469e0e8
Naron <naron.chen@richmond.edu>
2023-02-28 00:55:51 -0500

    format card & data

--- Film_Resource_Board/src/App.jsx
+++ Film_Resource_Board/src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Card from './components/Card'
+import films from './assets/filmdata.json'
 
 function App() {
 
@@ -7,8 +8,16 @@ function App() {
     <div className="App">
       <h1>Film Resource Board 🥑</h1>
         <div className="card-container">
-          < Card name='' />
-          {/* < Card /> */}
+          {films.map(film => (
+            <Card
+              key = {film.name}
+              name = {film.name}
+              rating = {film.rating}
+              // tags = {film.tags}
+              imgUrl = {film.imgUrl}
+              link = {film.link}
+            />
+          ))}
         </div>
     </div>
 
--- Film_Resource_Board/src/components/Card.jsx
+++ Film_Resource_Board/src/components/Card.jsx
@@ -1,14 +1,26 @@
 import React from "react";
 
-const Card = (name, description, rating, imageUrl, link) => {
+const Card = ({name, tags, rating, imgUrl, link}) => {
+    const renderStars = () => {
+        const fullstar_cnt = Math.floor(rating/2)
+        const halfStars_cnt = rating % 2 === 1 ? 1 : 0
+        const stars = [];
+        for (let i = 0; i < fullstar_cnt; i++){
+            stars.push(<img key={`full-star-${i}`} src="./src/assets/fullstar.jpeg" alt="Full star" width="40" height="40"/> )
+        }
+        for (let i = 0; i < halfStars_cnt; i++){
+            stars.push(<img key={`half-star-${i}`} src="./src/assets/halfstar.jpeg" alt="half star" width="40" height="40"/> )
+        }
+        return stars
+    }
     return (
         <div className="Card">
-            <img src={imageUrl} alt={name} className="film-card__image" />
-            <div className="film-card__content">
-                <h2 className="film-card__name">{name}</h2>
-                <p className="film-card__description">{description}</p>
-                <p className="film-card__rating">{rating}</p>
-                <a href={link} className="film-card__button button">Learn More</a>
+            <img src={imgUrl} alt={name} className="card__image" />
+            <div className="card__content">
+                <h2 className="card__name">{name}</h2>
+                <p className="card__tag">{tags}</p>
+                <p className="card__rating">{renderStars()}</p>
+                <a href={link} className="card__button">Learn More</a>
             </div>
         </div>
     )
