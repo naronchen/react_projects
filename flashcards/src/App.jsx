c01d69672a610f2a7cc532c76f32784e0fef0cd6
Naron <naron.chen@richmond.edu>
2023-03-07 02:56:02 -0500

    built basic flash card functions

--- flashcards/src/App.jsx
+++ flashcards/src/App.jsx
@@ -1,32 +1,54 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import data from './assets/data.json'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  //flipping logic
+  const [front, setfront] = useState(true)
+  const [idx, setidx] = useState(0)
+  const flip = () => {
+    let card_text = document.getElementById('card-text')
+    if (front){
+      card_text.innerHTML = data.answer[idx]
+    }
+    else{
+      card_text.innerHTML = data.question[idx]
+    }
+    setfront(!front)
+  }
+
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+  //prev & next 
+  const to_prev = () => {
+    if (idx > 0){ setidx(idx-1)}
+    else{ setidx(data.answer.length - 1)}
+  }
+  const to_next = () => {
+    if (idx < data.answer.length - 1){setidx(idx+1)}
+    else{ setidx(0)}
+  }
+
+  const get_random = () => {
+    const next = getRandomInt(data.answer.length-1)
+    console.log(next)
+    setidx(next)
+  }
+
+  //proceed logic
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="App" >
+      <div className="flashcard" onClick={flip}>
+        <h2 id="card-text">{data.question[idx]}</h2>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="button">
+        <h1 className="idx-button" onClick={to_prev}>prev</h1>
+        <h1 className="idx-button" onClick={get_random }>random</h1>
+        <h1 className="idx-button" onClick={to_next}>next</h1>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
