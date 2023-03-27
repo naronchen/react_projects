bb1cb8b5e85e2533d95123ca3a80eed7635863dc
Naron <naron.chen@richmond.edu>
2023-03-27 01:00:40 -0400

    add searchbar with fetch data functioning

---
+++ Drink_Detective/src/components/searchbar.jsx
@@ -0,0 +1,43 @@
+import { useState, useEffect } from "react";
+import AlcoholInfo from "../../../crypto-hustle/src/components/alcoholInfo"
+
+
+const SearchBar = () => {
+    const [searchInput, setSearchInput] = useState("");
+    const [searchResults, setSearchResults] = useState([]);
+
+    
+    const searchItems = searchValue => {
+        setSearchInput(searchValue);
+    }
+
+    useEffect(() => {
+        const fetchData = async () => {
+        try{
+            const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchInput}`)
+            const json = await response.json();
+            console.log(json)
+            setSearchResults(json);
+        }
+        catch(error){
+            console.log(error)
+        }
+    }
+    if (searchInput !== "") {
+        fetchData();
+      }
+    },[searchInput]);
+
+    return (
+        <div>
+            <input
+            type="text"
+            placeholder="Search..."
+            onChange={(inputString) => searchItems(inputString.target.value)}
+        />
+            <AlcoholInfo />
+        </div>
+    )
+}
+
+export default SearchBar;
