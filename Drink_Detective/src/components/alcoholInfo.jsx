88fced8303fa75bbfc14ba84a8e6f6aa0782df55
Naron <naron.chen@richmond.edu>
2023-03-27 19:51:53 -0400

    fixed rendering problem on fast delete

--- Drink_Detective/src/components/alcoholInfo.jsx
+++ Drink_Detective/src/components/alcoholInfo.jsx
@@ -1,7 +1,20 @@
 import '../components/alcoholInfo.css'
 const AlcoholInfo = ({ Info }) => {
-    if (Info.drinks) {
-        console.log("heelo", Info.drinks[0])
+    // if (Info.drinks) {
+    //     console.log("heelo", Info.drinks[0])
+    // }
+    const getIngredients = (drink) => {
+      let i = 1;
+      const ingredients = [];
+      while (`strIngredient${i}` in drink) {
+        const ingredient = `strIngredient${i}`;
+        if (!drink[ingredient]) {
+          break;
+        }
+        ingredients.push(drink[ingredient]);
+        i++;
+      }
+      return ingredients.join(', ');
     }
     
     return (
@@ -13,6 +26,7 @@ const AlcoholInfo = ({ Info }) => {
               <div className="drink-info">
                 <h3>{drink.strDrink}</h3>
                 <p>Category: {drink.strCategory}</p>
+                <p>Ingredients: {getIngredients(drink)}</p>
                 <p>Instructions: {drink.strInstructions}</p>
               </div>
             </li>
--- Drink_Detective/src/components/searchbar.jsx
+++ Drink_Detective/src/components/searchbar.jsx
@@ -4,7 +4,9 @@ import AlcoholInfo from "./alcoholInfo"
 
 const SearchBar = () => {
     const [searchInput, setSearchInput] = useState("");
+    console.log('searchInput: ', searchInput)
     const [searchResults, setSearchResults] = useState({});
+    console.log('searchResults: ', searchResults);
 
     
     const searchItems = searchValue => {
@@ -12,20 +14,31 @@ const SearchBar = () => {
     }
 
     useEffect(() => {
+        let isCancelled = false;
         const fetchData = async () => {
         try{
             const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchInput}`)
             const json = await response.json();
             console.log(json)
-            setSearchResults(json);
+            if (!isCancelled){
+                setSearchResults(json);
+            }
         }
         catch(error){
             console.log(error)
         }
     }
+
     if (searchInput !== "") {
         fetchData();
-      }
+    }
+    else{
+        setSearchResults({})
+    }
+    return () => {
+        isCancelled = true;
+    }
+    
     },[searchInput]);
 
     return (
@@ -35,7 +48,7 @@ const SearchBar = () => {
             placeholder="Search..."
             onChange={(inputString) => searchItems(inputString.target.value)}
         />
-            <AlcoholInfo Info = {searchResults}/>
+            { <AlcoholInfo Info = {searchResults}/>}
         </div>
     )
 }
